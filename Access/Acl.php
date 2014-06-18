9cec46f658e38f298bf54bbf3df76a11994bc800
Rodrigo Machado <rodrigo.machado@alemdisso.com.br>
2014-06-18 11:36:26 -0300

    acl administrator roles

--- Access/Acl.php
+++ Access/Acl.php
@@ -30,16 +30,44 @@ public function __construct() {
 
     $this->add(new Zend_Acl_Resource('moxca:admin'));
     $this->add(new Zend_Acl_Resource('moxca:admin.panel'));
+    $this->add(new Zend_Acl_Resource('moxca:admin.edition'));
+    $this->add(new Zend_Acl_Resource('moxca:admin.editor'));
+    $this->add(new Zend_Acl_Resource('moxca:admin.index'));
+    $this->add(new Zend_Acl_Resource('moxca:admin.post'));
+    $this->add(new Zend_Acl_Resource('moxca:admin.prize'));
+    $this->add(new Zend_Acl_Resource('moxca:admin.serie'));
+    $this->add(new Zend_Acl_Resource('moxca:admin.work'));
 
     $this->add(new Zend_Acl_Resource('moxca:blog'));
     $this->add(new Zend_Acl_Resource('moxca:blog.post'));
 
-    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.panel', 'panel');
+
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.panel', 'index');
 
     $this->allow(Moxca_Access_RolesConstants::ROLE_UNKNOWN,       'moxca:auth.login', 'index');
     $this->allow(Moxca_Access_RolesConstants::ROLE_UNKNOWN,       'moxca:auth.logout', 'index');
 
     $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:blog.post', 'explore-not-published');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.edition', 'create');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.edition', 'change-cover');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.edition', 'change-cover-designer');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.edition', 'change-illustrator');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.edition', 'change-isbn');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.edition', 'change-pages');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.edition', 'change-serie');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.edition', 'cover');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.editor', 'create');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.index', 'list-posts');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.index', 'list-works');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.work', 'change-description');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.work', 'change-summary');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.work', 'change-title');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.work', 'change-type');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.work', 'detail');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.work', 'editions-loop');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.work', 'list');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.work', 'prizes-loop');
+    $this->allow(Moxca_Access_RolesConstants::ROLE_ADMINISTRATOR, 'moxca:admin.work', 'remove');
 
   }
 
